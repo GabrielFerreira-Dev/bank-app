bbf8189b739e38d0d26d010c110711012011fe5f
Gabriel <gabrieel.ferreira55@gmail.com>
2026-03-25 00:35:21 -0300

    fix(hexagon): driven consuming and returning a concrete class instead dto

--- bank-hexagon/src/main/java/com/bank/hexagon/port/driven/AccountDrivenPort.java
+++ bank-hexagon/src/main/java/com/bank/hexagon/port/driven/AccountDrivenPort.java
@@ -1,13 +1,13 @@
 package com.bank.hexagon.port.driven;
 
-import com.bank.hexagon.domain.dto.AccountDTO;
+import com.bank.hexagon.domain.entity.Account;
 
 import java.util.UUID;
 
 public interface AccountDrivenPort {
-    AccountDTO save(AccountDTO accountDTO);
-    AccountDTO update(AccountDTO accountDTO);
-    AccountDTO findAccountById(UUID id);
+    Account save(Account account);
+    Account update(Account account);
+    Account findAccountById(UUID id);
     void transfer(UUID fromAccountId, UUID toAccountId, Double amount);
     void credit(UUID id, Double amount);
     void debit(UUID id, Double amount);
